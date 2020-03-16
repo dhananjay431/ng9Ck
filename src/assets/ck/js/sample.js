2a763ad8c826bf5c0c0405d36a453eed96b1b195
dbel431 <dbel431@gmail.com>
2020-03-16 14:18:53 +0530

    add all

---
+++ src/assets/ck/js/sample.js
@@ -0,0 +1,46 @@
+﻿
+
+var initSample = function () {
+	if (CKEDITOR.env.ie && CKEDITOR.env.version < 9)
+	CKEDITOR.tools.enableHtml5Elements(document);
+	CKEDITOR.config.height = 150;
+	CKEDITOR.config.width = 'auto';
+
+	var wysiwygareaAvailable = isWysiwygareaAvailable(),
+		isBBCodeBuiltIn = !!CKEDITOR.plugins.get('bbcode');
+	function isWysiwygareaAvailable() {
+		if (CKEDITOR.revision == ('%RE' + 'V%')) {
+			return true;
+		}
+		return !!CKEDITOR.plugins.get('wysiwygarea');
+	}
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'bidi', 'align', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+};
+	var editorElement = CKEDITOR.document.getById('editor');
+	if (isBBCodeBuiltIn) {
+		editorElement.setHtml("");
+	}
+	if (wysiwygareaAvailable) {
+		CKEDITOR.replace('editor');
+	} else {
+		editorElement.setAttribute('contenteditable', 'true');
+		CKEDITOR.inline('editor');
+	}
+}
