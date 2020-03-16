bcaf59f7c3552bc18c0b3d63789ce120d77b9257
dbel431 <dbel431@gmail.com>
2020-03-16 14:40:13 +0530

    add plug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var initSample:any,CKEDITOR:any;
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name = 'Angular';
+
+  ngOnInit(){
+    initSample();
+  }
+  getHtml(){
+      alert(CKEDITOR.instances.editor.getData());
+  }
+  
 }
